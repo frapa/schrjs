ca27f3205c84e1d785853b9de863a27eeea36701
Francesco Pasa <francescopasa@gmail.com>
2014-12-05 00:11:09 +0100

    quasi funziona

--- display.js
+++ display.js
@@ -1,53 +1,28 @@
+function array2csv(D, ef) {
+    var csv = "";
+    
+    for (var i = 0; i < D.length; i++) {
+        csv += D.get_value(i) + ",";
+        
+        for (var n = 0; n < ef.length; n++) {
+            csv += ef[n][i];
+            
+            if (n != ef.length - 1) {
+                csv += ",";
+            }
+        }
+        
+        csv += "\n";
+    }
+    
+    return csv;
+}
+
 //dominio, potential, eigenvalues, eigenfunction, computation time
 function display(D, V, ev, ef, t=0) {
-
-  g = new Dygraph(
-
-    // containing div
-    document.getElementById("graphdiv1sx"),
-
-    // CSV or path to a CSV file.
-    "Time,position\n" +
-    "-7,49\n" +
-    "-6,36\n" +
-    "-5,25\n" +
-    "-4,16\n" +
-    "-3,9\n" +
-    "-2,4\n" +
-    "-1,1\n" +
-    "0,0\n" +
-    "1,1\n" +
-    "2,4\n" +
-    "3,9\n" +
-    "4,16\n" +
-    "5,25\n" +
-    "6,36\n" +
-    "7,49\n"
-  );
-  
-  f = new Dygraph(
-
-    // containing div
-    document.getElementById("graphdiv1dx"),
-
-    // CSV or path to a CSV file.
-    "Time,position\n" +
-    "-7,-49\n" +
-    "-6,-36\n" +
-    "-5,-25\n" +
-    "-4,-16\n" +
-    "-3,-9\n" +
-    "-2,-4\n" +
-    "-1,-1\n" +
-    "0,-0\n" +
-    "1,-1\n" +
-    "2,-4\n" +
-    "3,-9\n" +
-    "4,-16\n" +
-    "5,-25\n" +
-    "6,-36\n" +
-    "7,-49\n"
-  );
+    g = new Dygraph(document.getElementById("graph"),
+        array2csv(D, ef));
+    //console.log(array2csv(D, ef));
 }
 
 function start_computation() {
